f4191933232142963f6211c665166e16faf44c73
Murilo <falconeri1501@gmail.com>
2024-02-12 17:36:04 -0300

    CRUD completo

--- controllers/users/comonController.js
+++ controllers/users/comonController.js
@@ -46,7 +46,6 @@ class ComonController{
 
     static async delete(req,res){
         try{
-            
             const {id} = req.params;
             const idObject = new ObjectId(id);
             //await db.collection('users').deleteOne({_id:idObject});
